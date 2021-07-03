7f2c9d4800c1352610e92f120e21b98293ad8f61
Fabio Mazzei <64652560+Fabio-Mazzei@users.noreply.github.com>
2021-07-02 22:02:07 -0300

    New exercise.
    /* Exercise 1. Print tables for 2 in the browser using JavaScript DOM to access HTML */

---
+++ exercise001Dom.js
@@ -0,0 +1,17 @@
+/* Exercise 1. Print tables for 2 in the browser using JavaScript DOM to access HTML */
+
+const bodyA = document.body
+const pA = document.createElement("p")
+bodyA.append(pA)
+pA.innerText = "2 Tables:"
+
+let num = 2
+let res
+let pb
+
+for (i = 0; i <= 10; i++) {
+    let pB = document.createElement("p")
+    bodyA.append(pB)
+    res = num * i
+    pB.innerText = `${num} x ${i} = ${res}`
+}
